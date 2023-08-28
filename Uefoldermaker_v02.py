f41a749b419357cd9994843fadb7348cfcd73652
dohazi70 <141938258+dohazi70@users.noreply.github.com>
2023-08-28 19:18:20 +0900

    Update Uefoldermaker_v02.py

--- Uefoldermaker_v02.py
+++ Uefoldermaker_v02.py
@@ -17,12 +17,20 @@ materialfunction_path = os.path.join(mastermaterial_path, "function")
 #blueprint
 Utility_path = os.path.join(project_content_path, "Utility")
 
+#work
+work_path = os.path.join(project_content_path, "work")
+dh_path = os.path.join(work_path, "dh_work")
+jb_path = os.path.join(work_path, "jb_work")
+yy_path = os.path.join(work_path, "yy_work")
+
 #shots
 shot_name = "sot"
 shot_count = int(20)
 shot_folder_path = os.path.join(project_content_path, "Shots")
 shot_lv_folder_path = os.path.join(shot_folder_path, "Shot_LV")
 os.makedirs(shot_folder_path, exist_ok=True)
+
+# 레벨 제작
 
 asset_tools = unreal.AssetToolsHelpers.get_asset_tools()
 level_factory = unreal.EditorLevelLibrary
@@ -32,10 +40,10 @@ level_path = "/Game/Levels/"
 
 
 for i in range(1, shot_count + 1):
-    shot_path = os.path.join(shot_lv_folder_path, f"{shot_name}_0{i}")
+    shot_path = os.path.join(shot_lv_folder_path, f"{shot_name}_{i:03d}")
     os.makedirs(shot_path)
     for j, level_name in enumerate(level_names):
-        full_level_name = f"{shot_name}_{i}_{level_suffixes[j]}"
+        full_level_name = f"{shot_name}_{i:03d}_{level_suffixes[j]}"
         print("full_level_name: " + full_level_name)
         full_level_path = os.path.join(shot_path, full_level_name)
         print("full_level_name: " + full_level_path)
@@ -67,9 +75,10 @@ for i in range(level_asset_data_len):
 
 unreal.EditorLevelLibrary.save_current_level()
 
+#레벨시퀀스 제작
 
 for i in range(1, shot_count + 1):
-    level_sequence = unreal.AssetTools.create_asset(asset_tools, asset_name = f"{shot_name}_{i}", package_path = "/Game/Shots/", asset_class = unreal.LevelSequence, factory = unreal.LevelSequenceFactoryNew())
+    level_sequence = unreal.AssetTools.create_asset(asset_tools, asset_name = f"{shot_name}_{i:03d}", package_path = "/Game/Shots/", asset_class = unreal.LevelSequence, factory = unreal.LevelSequenceFactoryNew())
     frame_rate = unreal.FrameRate(numerator = 24, denominator = 1)
     level_sequence.set_display_rate(frame_rate)
 
@@ -85,3 +94,6 @@ os.makedirs(nft_path)
 os.makedirs(materialfunction_path)
 os.makedirs(Utility_path)
 os.makedirs(movies_path)
+os.makedirs(dh_path)
+os.makedirs(jb_path)
+os.makedirs(yy_path)
