288cf82b0d3af07a04b0b49a8466fad1bd98c657
dohazi70 <leedohan70@gmail.com>
2023-09-20 17:07:35 +0900

    seq_rename

--- Utility/seq_rename.py
+++ Utility/seq_rename.py
@@ -4,28 +4,29 @@ from PySide2 import QtWidgets, QtGui, QtCore
 
 def replace():
     input_path = find_line.text()
-    input_verison = replace_line.text()
     parts = input_path.split("\\")
-    desired_part = "\\".join(parts[-2:])
+    desired_part = "\\".join(parts[-3:])
     master_path = desired_part.replace("\\", "_")
 
     file_list = os.listdir(input_path)
     print(file_list)
 
-    prefix = master_path + "_" + input_verison + "_"
+    prefix = master_path + "_"
     start_number = 0
 
     for file_name in file_list:
         file_path = os.path.join(input_path, file_name)
         file_extension = os.path.splitext(file_name)[-1]
         
-        new_file_name = f"{prefix}{start_number:03d}{file_extension}"
+        new_file_name = f"{prefix}{start_number:04d}{file_extension}"
         new_file_path = os.path.join(input_path, new_file_name)
         
         os.rename(file_path, new_file_path)
         start_number += 1
 
 
+
+#pyside 
 app = QtWidgets.QApplication.instance()
 if not app:
     app = QtWidgets.QApplication([])
@@ -42,13 +43,6 @@ find_layout.addWidget(find_label)
 find_line = QtWidgets.QLineEdit()
 find_layout.addWidget(find_line)
 layout.addLayout(find_layout)
-
-replace_layout = QtWidgets.QHBoxLayout()
-replace_label = QtWidgets.QLabel("verison:")
-replace_layout.addWidget(replace_label)
-replace_line = QtWidgets.QLineEdit()
-replace_layout.addWidget(replace_line)
-layout.addLayout(replace_layout)
 
 button = QtWidgets.QPushButton("Replace")
 button.clicked.connect(replace)
