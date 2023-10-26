2dfbc55af3897c9a3868d2cf6aa27204c107afb3
dohazi70 <leedohan70@gmail.com>
2023-10-26 18:55:12 +0900

    clarisse to unreal

---
+++ unreal/clarisse_to_unreal/make.py
@@ -0,0 +1,53 @@
+import unreal
+import re
+
+text  = """ 
+TextureMapFile {
+    name "snow_with_tall_grass_diffuse2"
+    #version 0.98
+    copy_from "build://project/layout/a/snow_mountain/detail/dini_snow_mtl/snow_with_tall_grass_diffuse2"
+    positions "mat_1" -1093 8721
+    projection 0
+    space 1
+    axis 1
+    object_space 2
+    uv_scale {
+        value 1 1 1
+        texture "build://project/layout/a/snow_mountain/detail/dini_snow_mtl/constant_color1"
+    }
+    filename "\\\\10.0.40.42\\user\\gen\\library\\sourceimages\\pbr\\snow\\CGAxis\\CGAxis PBR Textures Volume 12 - Snow\\snow_with_tall_grass\\4K\\tx\\snow_with_tall_grass_diffuse.tx"
+    frame_rate 24
+    color_space_auto_detect no
+    file_color_space "Clarisse|sRGB"
+}
+""" #입력 받아야하는 값
+
+def clarisse_TextureMapFile_data():
+    copy_from_match = re.search(r'copy_from "(.*?)"', text)
+    if copy_from_match:
+        copy_from = copy_from_match.group(1)
+    else:
+        copy_from = None
+    filename_match = re.search(r'filename "(.*?)"', text)
+    if filename_match:
+        filename = filename_match.group(1)
+    else:
+        filename = None
+    positions_match = re.search(r'positions "(.*?)"', text)
+    if positions_match:
+        positions = positions_match.group(1)
+    else:
+        positions = None
+
+def import_texture_task():
+
+    import_task = unreal.AssetImportTask()
+    import_task.filename = r"D:\Data\snow_with_tall_grass_diffuse.jpg"
+    import_task.destination_path = "/Game/layout/a/snow_mountain/datail/dini_snow_mtl/snow_with_tall_grass_diffuse2"
+    import_task.destination_name = 'snow_with_tall_grass_diffuse'
+    import_task.options = None # https://docs.unrealengine.com/5.3/en-US/PythonAPI/class/AssetImportTask.html#unreal.AssetImportTask
+
+    result = unreal.AssetToolsHelpers.get_asset_tools().import_asset_tasks([import_task])
+
+
+ 
