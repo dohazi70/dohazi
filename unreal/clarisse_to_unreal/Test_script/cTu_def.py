914a9002076c9f7e6589857502b2b17babcf758b
dohazi70 <leedohan70@gmail.com>
2023-11-08 16:42:48 +0900

    cTU material main add

--- unreal/clarisse_to_unreal/Test_script/cTu_def.py
+++ unreal/clarisse_to_unreal/Test_script/cTu_def.py
@@ -88,10 +88,8 @@ def import_unreal_texture(file_path, destination_path, texture_name):
     result = unreal.AssetToolsHelpers.get_asset_tools().import_asset_tasks([import_task]) 
 
 # 머트리얼 및 텍스쳐 연동
-
-import unreal
-def material_instance():
-    material_instance_name = "NewMaterialInstance2"
+def material_instance(mtl_instance_name, texture_path):
+    material_instance_name = mtl_instance_name
     material_instance_path = "/Game/Material/"
     parent_material_path = "/Game/Material/ParentMaterial"
 
@@ -104,7 +102,7 @@ def material_instance():
         material_instance.set_editor_property('Parent', parent_material)
 
         texture_parameter_name = "BaseColor"
-        texture_asset_path = "/Game/Material/texture/storage_lunar_diffC"
+        texture_asset_path = texture_path
 
         texture_asset = unreal.load_asset(texture_asset_path)
         if texture_asset and isinstance(texture_asset, unreal.Texture):
@@ -112,16 +110,76 @@ def material_instance():
 
         unreal.EditorAssetLibrary.save_loaded_asset(material_instance)
 
+#스태틱 메쉬 머트리얼 set
+import unreal
+
+# 스태틱 메시와 머티리얼의 경로를 지정합니다.
+static_mesh_path = '/Game/layout/car_cm/SM_car_cm.SM_car_cm'
+material_path = '/Game/layout/car_cm/car_cm/Looks/MI_M_car_cmSG_TwoSided'
+
+# 에셋 라이브러리를 사용하여 스태틱 메시와 머티리얼을 불러옵니다.
+static_mesh = unreal.EditorAssetLibrary.load_asset(static_mesh_path)
+material = unreal.EditorAssetLibrary.load_asset(material_path)
+
+# 스태틱 메시가 머티리얼을 설정할 수 있는 스태틱 메시 액터인지 확인합니다.
+if isinstance(static_mesh, unreal.StaticMesh) and isinstance(material, unreal.MaterialInterface):
+    # 스태틱 메시 액터에 대한 참조를 얻습니다.
+    # 예시에서는 스태틱 메시 액터가 이미 씬에 존재한다고 가정합니다.
+    # 이를 찾기 위해 액터의 이름이나 다른 속성을 사용할 수 있습니다.
+    static_mesh_actor = unreal.EditorLevelLibrary.spawn_actor_from_object(static_mesh, unreal.Vector(0, 0, 0))
+
+    # 스태틱 메시 컴포넌트에 접근합니다.
+    static_mesh_component = static_mesh_actor.get_static_mesh_component()
+
+    # 스태틱 메시 컴포넌트에 머티리얼을 설정합니다.
+    # 아래 코드는 메시의 첫 번째 머티리얼 슬롯에 머티리얼을 설정합니다.
+    static_mesh_component.set_material(0, material)
+
+    # 변경 사항을 저장합니다.
+    static_mesh_component.post_edit_change()
 
 
+#################################
+#메쉬 경로 찾기
+def find_static_meshes(search_string):
+    all_static_meshes = unreal.EditorAssetLibrary.list_assets('/Game/layout', recursive=True, include_folder=True)
+    
+    matching_meshes = []
+    for mesh in all_static_meshes:
+        if unreal.EditorAssetLibrary.does_asset_exist(mesh):
+            asset_name = mesh.split('/')[-1]
+            if asset_name.startswith(search_string + '.'):
+                matching_meshes.append(mesh)
+    return matching_meshes
 
+matching_meshes = find_static_meshes('SM_WR1')
+for mesh_path in matching_meshes:
+    print(mesh_path)
+############################
+#메트리얼 경로 찾기
+def find_material(search_string):
+    all_material = unreal.EditorAssetLibrary.list_assets('/Game/Material', recursive=True, include_folder=True)
+    
+    matching_material = []
+    for mesh in all_material:
+        if unreal.EditorAssetLibrary.does_asset_exist(mesh):
+            asset_name = mesh.split('/')[-1]
+            if asset_name.startswith(search_string + '.'):
+                matching_material.append(mesh)
+    return matching_material
 
+matching_material = find_material('SM_WR1_mtl')
+for material_path in matching_material:
+    print(material_path)
+############################
 
+import unreal
 
-
-
-
-
-
+static_mesh_path = '/Game/layout/car_cm/SM_car_cm.SM_car_cm'
+material_path = '/Game/layout/car_cm/car_cm/Looks/MI_M_car_cmSG.MI_M_car_cmSG'
+static_mesh = unreal.EditorAssetLibrary.load_asset(static_mesh_path)
+print(static_mesh)
+material = unreal.EditorAssetLibrary.load_asset(material_path)
+unreal.StaticMesh.set_material(static_mesh, 0, material)
 
 
--- unreal/clarisse_to_unreal/cTU_Material_main.py
+++ unreal/clarisse_to_unreal/cTU_Material_main.py
@@ -32,9 +32,43 @@ def material_instance(mtl_instance_name, texture_path):
             unreal.MaterialEditingLibrary.set_material_instance_texture_parameter_value(material_instance, unreal.Name(texture_parameter_name), texture_asset)
 
         unreal.EditorAssetLibrary.save_loaded_asset(material_instance)
+def find_static_meshes(search_string):
+    all_static_meshes = unreal.EditorAssetLibrary.list_assets('/Game/layout', recursive=True, include_folder=True)
+    
+    matching_meshes = []
+    for mesh in all_static_meshes:
+        if unreal.EditorAssetLibrary.does_asset_exist(mesh):
+            asset_name = mesh.split('/')[-1]
+            if asset_name.startswith(search_string + '.'):
+                matching_meshes.append(mesh)
+    return matching_meshes
+def find_material(search_string):
+    all_material = unreal.EditorAssetLibrary.list_assets('/Game/Material', recursive=True, include_folder=True)
+    
+    matching_material = []
+    for mesh in all_material:
+        if unreal.EditorAssetLibrary.does_asset_exist(mesh):
+            asset_name = mesh.split('/')[-1]
+            if asset_name.startswith(search_string + '.'):
+                matching_material.append(mesh)
+    return matching_material
+def process_json_data(Json_data):
+    all_matching_meshes = []
+    all_matching_materials = []
 
+    for item_dict in Json_data:
+        for key in item_dict:
+            mesh_value = item_dict[key]['mesh']
+            mat2_value = item_dict[key]['mat']
 
+            matching_meshes = find_static_meshes(mesh_value)
+            matching_materials = find_material(mat2_value)
 
+            all_matching_meshes.extend(matching_meshes)  # 결과를 전체 리스트에 추가합니다.
+            all_matching_materials.extend(matching_materials)  # 결과를 전체 리스트에 추가합니다.
+
+    # 모든 처리가 끝난 후, 메시 리스트와 재질 리스트를 반환합니다.
+    return all_matching_meshes, all_matching_materials
 
 Json_file_path = r'D:\dev\Code\dohazi\unreal\clarisse_to_unreal\Test_script\test.json'
 
@@ -50,12 +84,9 @@ unique_mats = []
 for item_dict in Json_data:
     for key in item_dict:
         dif_value = item_dict[key]['dif']
+        mat_value = item_dict[key]['mat']
         if dif_value not in unique_difs:
             unique_difs.append(dif_value)
-
-for item_dict in Json_data:
-    for key in item_dict:
-        mat_value = item_dict[key]['mat']
         if mat_value not in unique_mats:
             unique_mats.append(mat_value)
 
@@ -63,8 +94,15 @@ for dif in unique_difs:
     dif_extension = os.path.splitext(os.path.basename(dif))[0]
     dif_base_name = re.sub(r'\.\d+$', '', dif_extension)
     import_unreal_texture(dif, ue_texture_path, dif_base_name)
-    
+
 for mat in unique_mats:
     dif_local_path = ue_texture_path + "/" + dif_base_name
     material_instance(mat, dif_local_path)
+
+matching_meshes, matching_materials = process_json_data(Json_data)
+
+for meshe_path, materials_path in zip(matching_meshes, matching_materials):
+    static_mesh = unreal.EditorAssetLibrary.load_asset(meshe_path)
+    material = unreal.EditorAssetLibrary.load_asset(materials_path)
+    unreal.StaticMesh.set_material(static_mesh, 0, material)
 
